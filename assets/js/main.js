b96b43629cee7173a63bbcdc83e526f6aa054382
ThienBui1801 <buiquythien1801@gmail.com>
2022-02-26 15:32:30 +0700

    update function click

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,22 +3,72 @@
 // open close modal
 const openModalHeader = document.querySelector('.navbar-toggler');
 const closeBtn = document.querySelector('.modal__header-close');
-const modal = document.querySelector('.modal__header');2
+const modal = document.querySelector('.modal__header');
 const modalContainer = document.querySelector('.modal__header-container');
 
 function showModalHeader() {
-  modal.classList.add('open')
+  modal.classList.add('open');
 }
 
 function hideModalHeader() {
-  modal.classList.remove('open')
+  modal.classList.remove('open');
 }
 
 openModalHeader.addEventListener('click', showModalHeader);
 closeBtn.addEventListener('click', hideModalHeader);
 modal.addEventListener('click', hideModalHeader);
-modalContainer.addEventListener('click', function(event) {
+modalContainer.addEventListener('click', function (event) {
   event.stopPropagation()
 });
 
 
+// open sub modal
+const btnOpenHome = document.querySelector('.js-home');
+const btnOpenServices = document.querySelector('.js-services');
+const btnOpenPages = document.querySelector('.js-pages');
+const btnOpenBlock = document.querySelector('.js-block');
+
+function showSubMenuHome() {
+  let menuOpen = document.getElementById('sub-home');
+  if (menuOpen.style.display === 'none') {
+    menuOpen.style.display = 'block';
+  } else {
+    menuOpen.style.display = 'none';
+  }
+  // subMenu.classList.add('open');
+};
+
+function showSubMenuServices() {
+  let menuOpen = document.getElementById('sub-services');
+  if (menuOpen.style.display === 'none') {
+    menuOpen.style.display = 'block';
+  } else {
+    menuOpen.style.display = 'none';
+  }
+  // subMenu.classList.add('open');
+};
+
+function showSubMenuPages() {
+  let menuOpen = document.getElementById('sub-pages');
+  if (menuOpen.style.display === 'none') {
+    menuOpen.style.display = 'block';
+  } else {
+    menuOpen.style.display = 'none';
+  }
+  // subMenu.classList.add('open');
+};
+
+function showSubMenuBlock() {
+  let menuOpen = document.getElementById('sub-block');
+  if (menuOpen.style.display === 'none') {
+    menuOpen.style.display = 'block';
+  } else {
+    menuOpen.style.display = 'none';
+  }
+  // subMenu.classList.add('open');
+};
+
+btnOpenHome.addEventListener('click', showSubMenuHome);
+btnOpenServices.addEventListener('click', showSubMenuServices);
+btnOpenPages.addEventListener('click', showSubMenuPages);
+btnOpenBlock.addEventListener('click', showSubMenuBlock);
